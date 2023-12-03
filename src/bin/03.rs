805a04a7aab6ec42429d2f03cc2143c6e28cf074
Andrea Carpino <carpocci@gmail.com>
2023-12-03 23:29:15 +0100

    Solution for day 3

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,65 +1,55 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(3);
 
-fn set_true(mut symbols_table: Vec<Vec<bool>>, i: usize, j: usize) -> Vec<Vec<bool>> {
-    if i > 0 {
-        if j > 0 {
-            symbols_table[i - 1][j - 1] = true;
-        }
-        symbols_table[i - 1][j] = true;
-        symbols_table[i - 1][j + 1] = true;
-    }
-
-    if j > 0 {
-        symbols_table[i][j - 1] = true;
-    }
-    symbols_table[i][j] = true;
-    symbols_table[i][j + 1] = true;
-
-    if j > 0 {
-        symbols_table[i + 1][j - 1] = true;
-    }
-    symbols_table[i + 1][j] = true;
-    symbols_table[i + 1][j + 1] = true;
-
-    symbols_table
-}
-
-pub fn part_one(input: &str) -> Option<u32> {
+fn generate(
+    input: &str,
+    function: &dyn Fn(char) -> bool,
+) -> (Vec<Vec<char>>, Vec<Vec<u32>>, usize) {
     let mut map: Vec<Vec<char>> = input
-        .replace("\n", ".\n")
+        .replace('\n', ".\n")
         .split_terminator('\n')
         .map(|row| row.chars().collect())
         .collect();
 
-    let columns = map[0].len();
-    map.push((0..columns).map(|_| '.').collect());
-    let rows = map.len();
+    let size = map[0].len();
+    map.push(vec!['.'; size]);
+    map.insert(0, vec!['.'; size]);
 
-    let mut symbols_table: Vec<Vec<bool>> = (0..rows)
-        .map(|_i| (0..columns).map(|_j| false).collect())
-        .collect();
+    let mut values_table: Vec<Vec<u32>> = vec![vec![0; size]; size];
 
-    for i in 0..rows {
-        for j in 0..columns {
-            if !(map[i][j].is_numeric() || map[i][j] == '.') {
-                symbols_table = set_true(symbols_table, i, j);
+    let mut id = 1;
+    for (i, row) in map.iter().enumerate() {
+        for (j, cell) in row.iter().enumerate() {
+            if function(*cell) {
+                for &i in &[i - 1, i, i + 1] {
+                    for &j in &[j - 1, j, j + 1] {
+                        values_table[i][j] = id;
+                    }
+                }
+                id += 1;
             }
         }
     }
+
+    (map, values_table, size)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (map, values_table, size) = generate(input, &|n| !(n.is_numeric() || n == '.'));
 
     let mut number = 0;
     let mut take = false;
 
     let mut sum = 0;
 
-    for i in 0..rows {
-        for j in 0..columns {
-            let cell = map[i][j].to_digit(10);
-            match cell {
+    for i in 0..size {
+        for j in 0..size {
+            match map[i][j].to_digit(10) {
                 Some(n) => {
                     number *= 10;
                     number += n;
-                    if symbols_table[i][j] {
+                    if values_table[i][j] != 0 {
                         take = true;
                     }
                 }
@@ -67,7 +57,6 @@ pub fn part_one(input: &str) -> Option<u32> {
                     if number != 0 {
                         if take {
                             sum += number;
-                        } else {
                         }
                         take = false;
                         number = 0;
@@ -80,8 +69,44 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (map, values_table, size) = generate(input, &|n| !(n.is_numeric() || n == '.'));
+
+    let mut number = 0;
+    let mut gear_id = 0;
+
+    let mut gears = HashMap::<u32, Vec<u32>>::new();
+
+    for i in 0..size {
+        for j in 0..size {
+            match map[i][j].to_digit(10) {
+                Some(n) => {
+                    number *= 10;
+                    number += n;
+                    if values_table[i][j] != 0 {
+                        gear_id = values_table[i][j];
+                    }
+                }
+                None => {
+                    if number != 0 {
+                        if gear_id != 0 {
+                            gears.entry(gear_id).or_default().push(number);
+                        }
+                        gear_id = 0;
+                        number = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    Some(
+        gears
+            .iter()
+            .filter(|(_, v)| (**v).len() > 1)
+            .map(|(_, v)| (*v).iter().product::<u32>())
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -97,6 +122,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
